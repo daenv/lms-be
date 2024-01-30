3eac4da82a7615522b6ce21927150dffc8e8adca
daenv <daenvle69@gmail.com>
2024-01-30 22:50:40 +0700

    Add createUser method to UsersService

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { EntityManager, Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
-      @InjectRepository() 
+    @InjectRepository(User) private repositoryUser: Repository<User>,
+    private readonly entityManager: EntityManager,
   ) {}
+
+  async createUser(createUserDto: CreateUserDto) {
+    const user = new User({
+      ...createUserDto,
+    });
+    try {
+      const userCreated = await this.entityManager.save(user);
+      if (!userCreated) {
+        throw new BadRequestException('User not created');
+      }
+      return userCreated;
+    } catch (error) {}
+  }
 }
