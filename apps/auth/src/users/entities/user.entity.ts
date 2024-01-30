835378e789166bcfb07a75303eff237fbb17f11d
daenv <daenvle69@gmail.com>
2024-01-30 22:50:49 +0700

    Add validation decorators to User entity

--- apps/auth/src/users/entities/user.entity.ts
+++ apps/auth/src/users/entities/user.entity.ts
@@ -1,8 +1,32 @@
 import { AbstractEntity } from '@app/common';
+import { Exclude } from 'class-transformer';
+import { IsEmail, IsString } from 'class-validator';
 import { Column, Entity } from 'typeorm';
 
 @Entity()
 export class User extends AbstractEntity<User> {
   @Column()
+  @IsString()
   username: string;
+
+  @Column()
+  @IsString()
+  @Exclude()
+  password: string;
+
+  @Column()
+  @IsEmail()
+  email: string;
+
+  @Column()
+  @IsString()
+  firstName: string;
+
+  @Column()
+  @IsString()
+  lastName: string;
+
+  @Column()
+  @IsString()
+  phone: string;
 }
