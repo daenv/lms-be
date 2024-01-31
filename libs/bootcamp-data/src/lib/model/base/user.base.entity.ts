2dd7f1bde7ddbc146728075fd7f52cfb52a14a48
daenv <daenvle69@gmail.com>
2024-01-31 11:11:38 +0700

    Add UserBaseEntity class

---
+++ libs/bootcamp-data/src/lib/model/base/user.base.entity.ts
@@ -0,0 +1,8 @@
+import { Column, Index } from 'typeorm';
+import { IdEntity } from './id.entity';
+
+export abstract class UserBaseEntity extends IdEntity {
+  @Index()
+  @Column({ type: 'uuid' })
+  userId: string;
+}
