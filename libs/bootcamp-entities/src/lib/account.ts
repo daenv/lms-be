a09fa361ff78c300448f6e516c24701f546ea169
daenv <daenvle69@gmail.com>
2024-01-31 10:34:41 +0700

    Add Account entity class

---
+++ libs/bootcamp-entities/src/lib/account.ts
@@ -0,0 +1,48 @@
+import { AbstractEntity } from '@app/common';
+import { IsEmail, IsString } from 'class-validator';
+import { Column } from 'typeorm';
+
+export class Account extends AbstractEntity<Account> {
+  @Column()
+  public username: string;
+
+  @Column()
+  @IsEmail()
+  public email: string;
+  @Column()
+  public password: string;
+
+  @Column()
+  @IsString()
+  public firstName: string;
+
+  @Column()
+  @IsString()
+  public lastName: string;
+
+  @Column()
+  public phone: string;
+
+  @Column()
+  public pw: string;
+  @Column()
+  public pwSame: string;
+  @Column()
+  public jwtToken: string;
+
+  @Column()
+  public refreshToken: string;
+
+  @Column()
+  public roles: string[];
+  @Column()
+  public verification: string;
+  @Column()
+  public verified: boolean;
+  @Column()
+  public verificationExpires: Date;
+  @Column()
+  public loginAttempts?: number;
+  @Column()
+  public blockExpires?: Date;
+}
