e57fba0131a900bae459b5ea1ca29852fa015626
daenv <daenvle69@gmail.com>
2024-01-31 11:34:37 +0700

    Refactor Account class properties
    This commit refactors the properties of the Account class in `account.ts`. The changes include removing the `password` property, renaming `pw` to `pwSalt`, and updating the `pwSame` property to `pwSalt`.

--- libs/bootcamp-entities/src/lib/account.ts
+++ libs/bootcamp-entities/src/lib/account.ts
@@ -1,14 +1,12 @@
-import { AbstractEntity } from '@app/common';
-
-export class Account extends AbstractEntity<Account> {
+export class Account {
+  public id?: string;
   public username: string;
   public email: string;
-  public password: string;
   public firstName: string;
   public lastName: string;
   public phone: string;
   public pw: string;
-  public pwSame: string;
+  public pwSalt: string;
   public jwtToken: string;
   public refreshToken: string;
   public roles: string[];
