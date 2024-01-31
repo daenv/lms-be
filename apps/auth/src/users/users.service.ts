faed2bb07ed59c999f4678f18537c389f08d6556
daenv <daenvle69@gmail.com>
2024-01-31 09:07:30 +0700

    Update import statement in users.service.ts

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
+import { User } from '@app/common';
 import { EntityManager, Repository } from 'typeorm';
 import { CreateUserDto } from './dtos/create-user.dto';
 
