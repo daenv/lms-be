55f4d5fdb0e53f430908e8cbc951bb283404a076
daenv <daenvle69@gmail.com>
2024-02-01 19:50:51 +0700

    Add class-validator, cookie-parser, and global pipes

--- apps/accounts/src/main.ts
+++ apps/accounts/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AccountsModule } from './accounts.module';
+import { useContainer } from 'class-validator';
+import * as cookiesParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AccountsModule);
-  await app.listen(3000);
+  useContainer(app, { fallbackOnErrors: true });
+  app.enableCors();
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  // app.connectMicroservice({
+  //   transport: Transport.TCP,
+  //   options: {
+  //     host: '0.0.0.0',
+  //     port: configService.get('TCP_PORT'),
+  //   },
+  // });
+
+  // app.use(cookiesParser());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const port = process.env.PORT || 3333;
+  await app.listen(port, () => {
+    console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+  });
 }
 bootstrap();
