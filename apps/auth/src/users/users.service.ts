d9b29720c0b4755862cc34ed6b5d5908a90a7a3b
daenv <daenvle69@gmail.com>
2024-02-01 08:53:39 +0700

    Refactor UsersService constructor and remove unused code

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,26 +1,10 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Account } from '@app/bootcamp-entities';
-import { EntityManager, Repository } from 'typeorm';
-import { CreateUserDto } from './dtos/create-user.dto';
+import { AccountRepository } from '@app/bootcamp-data';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
-  constructor(
-    @InjectRepository(Account) private repositoryUser: Repository<Account>,
-    private readonly entityManager: EntityManager,
-  ) {}
+  constructor(private readonly usersRepository: AccountRepository) {}
 
-  // async createUser(createUserDto: CreateUserDto) {
-  //   const user = new Account({
-  //     ...createUserDto,
-  //   });
-  //   try {
-  //     const userCreated = await this.entityManager.save(user);
-  //     if (!userCreated) {
-  //       throw new BadRequestException('User not created');
-  //     }
-  //     return userCreated;
-  //   } catch (error) {}
-  // }
+  private async validateCreateUserDto() {}
+  async create() {}
 }
