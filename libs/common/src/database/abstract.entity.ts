d3e09976d880eaaa5e8f016bec80d079fed0018c
daenv <daenvle69@gmail.com>
2024-02-01 10:22:42 +0700

    Remove unused TypeORM configuration and abstract entity

--- libs/common/src/database/abstract.entity.ts
+++
@@ -1,9 +0,0 @@
-import { PrimaryGeneratedColumn } from 'typeorm';
-
-export class AbstractEntity<T> {
-  @PrimaryGeneratedColumn()
-  public id?: number;
-  constructor(entity: Partial<T>) {
-    Object.assign(this, entity);
-  }
-}
