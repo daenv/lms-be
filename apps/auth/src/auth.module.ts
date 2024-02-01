55c690999d4dc0d3b58200a5fae8bdfa4f0104cd
daenv <daenvle69@gmail.com>
2024-02-01 09:10:54 +0700

    Remove UsersModule import from auth.module.ts

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -3,14 +3,9 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from '@app/common';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    DatabaseModule,
-    UsersModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
