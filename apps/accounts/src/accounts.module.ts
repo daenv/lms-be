f86e90b6d388bd7fd788eaeaef5443ba6c634342
daenv <daenvle69@gmail.com>
2024-02-01 10:41:04 +0700

    Add database and config modules to accounts module

--- apps/accounts/src/accounts.module.ts
+++ apps/accounts/src/accounts.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AccountsController } from './accounts.controller';
 import { AccountsService } from './accounts.service';
+import { DatabaseModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+  ],
   controllers: [AccountsController],
   providers: [AccountsService],
 })
