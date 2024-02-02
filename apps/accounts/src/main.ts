7d2c923d064c1155da4baddaed743982242c5b81
daenv <daenvle69@gmail.com>
2024-02-02 10:25:41 +0700

    Add Swagger documentation for production environment

--- apps/accounts/src/main.ts
+++ apps/accounts/src/main.ts
@@ -3,6 +3,8 @@ import { AccountsModule } from './accounts.module';
 import { useContainer } from 'class-validator';
 // import * as cookiesParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import { configService } from '@app/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AccountsModule);
@@ -21,6 +23,20 @@ async function bootstrap() {
 
   // app.use(cookiesParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  if (configService.isProduction()) {
+    const option = new DocumentBuilder()
+      .setTitle('Production Code Bootcamp API')
+      .setDescription('Code Bootcamp API')
+      .setVersion('1.0')
+      .addTag('API')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, option, {
+      include: [AccountsModule],
+    });
+    SwaggerModule.setup('api', app, document);
+  }
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
