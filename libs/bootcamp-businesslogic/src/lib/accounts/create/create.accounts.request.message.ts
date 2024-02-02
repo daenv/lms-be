7d5076026075fefa381af2786255d0b1016ef051
daenv <daenvle69@gmail.com>
2024-02-02 10:26:14 +0700

    Add CreateAccountsRequestMessage class

---
+++ libs/bootcamp-businesslogic/src/lib/accounts/create/create.accounts.request.message.ts
@@ -0,0 +1,7 @@
+export class CreateAccountsRequestMessage {
+  public firstName: string;
+  public lastName: string;
+  public password: string;
+  public passwordRepeat: string;
+  public email: string;
+}
