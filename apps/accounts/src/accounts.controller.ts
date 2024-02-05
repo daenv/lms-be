e098e6a77b9c1a35469b4dccc6bb4b3122edce67
daenv <daenvle69@gmail.com>
2024-02-05 14:13:28 +0700

    Add error interceptor to register endpoint

--- apps/accounts/src/accounts.controller.ts
+++ apps/accounts/src/accounts.controller.ts
@@ -1,13 +1,23 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Res,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { Response } from 'express';
 import { CreateAccountDto } from './dto/create-account.dto';
+import { CreateAccountFactory } from './api.create/create.accounts.factory';
+import { AccountsErrorInterceptor } from './common';
 
 @Controller()
 export class AccountsController {
   constructor(private readonly accountsService: AccountsService) {}
 
   @Post()
+  @UseInterceptors(AccountsErrorInterceptor)
   public async register(
     @Res() res: Response,
     @Body() createAccountDto: CreateAccountDto,
