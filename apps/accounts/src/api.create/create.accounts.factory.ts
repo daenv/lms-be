a438cef5496692d719594459b7b0cbdecb948399
daenv <daenvle69@gmail.com>
2024-02-15 16:19:38 +0700

    Delete CreateAccountFactory class

--- apps/accounts/src/api.create/create.accounts.factory.ts
+++
@@ -1,7 +0,0 @@
-import { IControllerFactory } from '../shared/interfaces/controller-factory.interface';
-
-export class CreateAccountFactory extends IControllerFactory<any> {
-  public create() {
-    throw new Error('Method not implemented.');
-  }
-}
