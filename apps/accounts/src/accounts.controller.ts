280efad55581b3b24c3d2d1c277795fd236cb9ef
daenv <daenvle69@gmail.com>
2024-02-15 16:21:47 +0700

    Remove unused import in accounts.controller.ts

--- apps/accounts/src/accounts.controller.ts
+++ apps/accounts/src/accounts.controller.ts
@@ -9,7 +9,6 @@ import {
 import { AccountsService } from './accounts.service';
 import { Response } from 'express';
 import { CreateAccountDto } from './dto/create-account.dto';
-import { CreateAccountFactory } from './api.create/create.accounts.factory';
 import { AccountsErrorInterceptor } from './common';
 
 @Controller()
