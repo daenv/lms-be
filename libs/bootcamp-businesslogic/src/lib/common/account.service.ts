0c8820c7d51b08ad65993a957c3358ce2b64afdc
daenv <daenvle69@gmail.com>
2024-02-17 15:20:44 +0700

    Add CreateAccountsRequestMessage and EncryptUser imports, and implement execUseCase and isEmailUnique methods in AccountService

--- libs/bootcamp-businesslogic/src/lib/common/account.service.ts
+++ libs/bootcamp-businesslogic/src/lib/common/account.service.ts
@@ -3,7 +3,12 @@ import {
   Consts,
   IRepository,
   IVerificaionGeneratorDependencies,
+  Messages,
 } from '@app/bootcamp-entities';
+import { CreateAccountsRequestMessage } from '../accounts';
+import { EncryptUser } from './encrypt.user';
+import {} from 'class-validator';
+import { ValidationException } from './exceptions/validation.exception.interface';
 
 export class AccountSerivce {
   HOURS_TO_VERIFY = Consts.HOURS_TO_VERIFY;
@@ -15,5 +20,30 @@ export class AccountSerivce {
   ) {
     this._repository = _repository;
     this._verificationGenerator = _verificationGenerator;
+  }
+
+  public async execUseCase(
+    request: CreateAccountsRequestMessage,
+  ): Promise<Account> {
+    // handle request from the server
+    const account = new Account();
+    account.firstName = request.firstName;
+    account.lastName = request.lastName;
+    account.pw = request.password;
+    account.email = request.email;
+
+    const encrypUser = new EncryptUser();
+
+    await encrypUser.encryptUser(account);
+    //     await this.is
+
+    return await this._repository.save(account);
+  }
+
+  public async isEmailUnique(email: string) {
+    const user = await this._repository.getByQuery({ email });
+    if (user) {
+      throw new ValidationException(Messages.EMAIL_MUST_BE_UNIQUE);
+    }
   }
 }
