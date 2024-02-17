31526ce5669b6bca02c3ea4be01bd5cbf1ffcf59
daenv <daenvle69@gmail.com>
2024-02-18 00:42:22 +0700

    Add AccountsHelper class for password validation

---
+++ libs/bootcamp-businesslogic/src/lib/common/account.helper.ts
@@ -0,0 +1,14 @@
+import { Consts, Messages } from '@app/bootcamp-entities';
+import { isLength } from 'validator';
+const MIN_PASSWORD_LENGTH = Consts.MIN_PASSWORD_LENGTH;
+const IP_INVALID = Messages.IP_INVALID;
+const COUNTRY_INVALID = Messages.COUNTRY_INVALID;
+const BROWSER_INVALID = Messages.BROWSER_INVALID;
+const MIN_BROWSER_LENGTH = Consts.MIN_BROWSER_LENGTH;
+const MIN_COUNTRY_LENGTH = Consts.MIN_COUNTRY_LENGTH;
+
+export class AccountsHelper {
+  public static isPassword(password: string) {
+    return isLength(password, MIN_PASSWORD_LENGTH);
+  }
+}
