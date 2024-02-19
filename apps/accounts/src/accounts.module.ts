b7e6d87474c009b1c14234a64767acd88a65a33c
daenv <daenvle69@gmail.com>
2024-02-19 09:06:28 +0700

    Remove AccountsService and add AccountSerivce

--- apps/accounts/src/accounts.module.ts
+++ apps/accounts/src/accounts.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AccountsController } from './accounts.controller';
-import { AccountsService } from './accounts.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from '@app/common';
 import { AccountEntity } from '@app/bootcamp-data';
+import { AccountSerivce } from '@app/bootcamp-businesslogic';
 
 @Module({
   imports: [
@@ -11,6 +11,6 @@ import { AccountEntity } from '@app/bootcamp-data';
     TypeOrmModule.forFeature([AccountEntity]),
   ],
   controllers: [AccountsController],
-  providers: [AccountsService],
+  providers: [AccountSerivce],
 })
 export class AccountsModule {}
